1b1808c2d69ca11e813ad49d342aa04b7add9882
Yusuke Kawasaki <u-suke@kawa.net>
2016-11-17 22:37:23 +0900

    CLI: -JCjc is required

--- bin/jaccard-stream.js
+++ bin/jaccard-stream.js
@@ -4,20 +4,6 @@ var fs = require("fs");
 var JaccardStream = require("../");
 
 function CLI(opts, input, output) {
-  var first = opts && opts[0];
-
-  if (first !== "-") {
-    var cmd = process.argv[1].split("/").pop();
-    console.warn("Usage: " + cmd + " [-CJcjm] [input] [output]");
-    console.warn("  C: input format is CSV file");
-    console.warn("  J: input format is JSON stream (ndjson)");
-    console.warn("  c: output format is CSV file");
-    console.warn("  j: output format is JSON stream (ndjson)");
-    console.warn("  N: detect column names from the first line of input CSV");
-    console.warn("  n: output with named columns");
-    process.exit(1);
-  }
-
   var optI = {};
   var optJ = {};
   var optO = {};
@@ -26,7 +12,8 @@ function CLI(opts, input, output) {
   if (input === "-") input = null;
   if (output === "-") output = null;
 
-  opts.split("").forEach(function(c) {
+  var first = opts && opts[0];
+  if (first === "-") opts.split("").forEach(function(c) {
     param[c] = 1;
   });
   if (param.N) {
@@ -37,13 +24,30 @@ function CLI(opts, input, output) {
     optO.header = true;
   }
 
+  var parse;
+  if (param.J) parse = _require("ndjson").parse(optI);
+  if (param.C) parse = _require("csv").parse(optI);
+
+  var stringify;
+  if (param.j) stringify = _require("ndjson").stringify(optO);
+  if (param.c) stringify = _require("csv").stringify(optO);
+
+  if (!parse || !stringify) {
+    var cmd = process.argv[1].split("/").pop();
+    console.warn("Usage: " + cmd + " [-CJcjNn] [input] [output]");
+    console.warn("");
+    console.warn("  C: input format is CSV file");
+    console.warn("  J: input format is JSON stream (ndjson)");
+    console.warn("  c: output format is CSV file");
+    console.warn("  j: output format is JSON stream (ndjson)");
+    console.warn("  N: detect column names from the first line of input CSV");
+    console.warn("  n: output with named columns");
+    process.exit(1);
+  }
+
   var read = input ? fs.createReadStream(input, "utf-8") : process.stdin;
 
-  var parse = param.J ? _require("ndjson").parse(optI) : _require("csv").parse(optI);
-
   var jaccard = JaccardStream(optJ);
-
-  var stringify = param.j ? _require("ndjson").stringify(optO) : _require("csv").stringify(optO);
 
   var write = output ? fs.createWriteStream(output) : process.stdout;
 
